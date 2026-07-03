8c2dcc0c3507bc6129282c70dc16e4f7aac10ff9
pramodhini-23 <pramodhini1223@gmail.com>
2026-07-03 17:06:41 +0530

    changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,7 +1,7 @@
 AUTHOR = 'Pramodhini'
 SITENAME = 'Pramodhini'
 SITETITLE = 'Pramodhini'
-SITEURL = "https://pramodhini-23.github.io/pramodhini"
+SITEURL = ""
 PATH = "content"
 TIMEZONE = 'Asia/Kolkata'
 DEFAULT_LANG = 'en'
